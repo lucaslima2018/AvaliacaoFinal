db92494563257c217fa709f451504024b7f03b58
lucaslima2018 <33165385+lucaslima2018@users.noreply.github.com>
2019-07-13 20:21:14 -0300

    Add files via upload

---
+++ XML.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Linq;
+using System.Xml.Linq;
+
+namespace AvaliacaoFinal {
+    public class XML {
+
+    }
+
+    public bool ReadUsingLinqNameSelect(string productName) {
+        XDocument doc = XDocument.Load("PRODUCTS.xml");
+        var query = from c in doc.Element("Products").Elements("Product")
+                    where c.Element("Product_name").Value == productName
+                    select c;
+
+        return query.Count() > 0;
+
+    }
+}
